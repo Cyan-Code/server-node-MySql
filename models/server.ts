19d88936802925458f9d8bac4b9bee508f2d33c8
Luis Carrion <lcarrionalvarez@gmail.com>
2022-02-07 17:05:20 -0500

    Sequelize + cadena de coneccion

--- models/server.ts
+++ models/server.ts
@@ -2,6 +2,8 @@ import express, {Application} from 'express';
 import * as userRoute from '../routes/user';
 import cors from 'cors';
 
+import db from '../db/connection';
+
 class Server {
   private app:Application;
   private port:string;
@@ -12,9 +14,20 @@ class Server {
   constructor ( ) {
     this.app = express();
     this.port = process.env.PORT || '8000';
-
+    this.dbConnection();
     this.middlewares();
     this.routes();
+  }
+
+  async dbConnection(){
+    try {
+      
+      await db.authenticate();
+      console.log('db online');
+
+    } catch (error) {
+      throw new Error(`${error}: Osea, efesota`);
+    }
   }
 
   middlewares(){
