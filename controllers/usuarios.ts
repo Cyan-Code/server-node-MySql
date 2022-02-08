febd7a95ba0fb84b224df8d271aaf1024961af2a
Luis Carrion <lcarrionalvarez@gmail.com>
2022-02-07 19:10:38 -0500

    Update and create with Sequelize

--- controllers/usuarios.ts
+++ controllers/usuarios.ts
@@ -1,43 +1,74 @@
 import { Response, Request } from "express";
+import Usuario from "../models/user";
 
-export const getUsuarios = (req:Request, res:Response) => {
-
+export const getUsuarios = async (req:Request, res:Response) => {
+  const usuarios = await Usuario.findAll();
   res.json({
-    msg: 'Get Usuarios'
+    msg: 'Get Usuarios',
+    usuarios
   })
 
 }
 
-export const getUsuario = (req:Request, res:Response) => {
+export const getUsuario = async (req:Request, res:Response) => {
 
   const { id } = req.params;
-
-  res.json({
-    msg: 'Get Usuario',
-    id
-  })
-
+  const usuario = await Usuario.findByPk(id);
+  if (usuario) {
+    return res.json(usuario)
+  } else {
+    return res.status(404).json({
+      msg: `No existe un usuario con el ${id}`
+    })
+  }
 }
 
-export const postUsuario = (req:Request, res:Response) => {
+export const postUsuario = async (req:Request, res:Response) => {
   const { body } = req;
-  console.log(req.body);
-  res.json({
-    msg: 'post Usuario',
-    body
-  })
+  try {
+    const existeEmail = await Usuario.findOne({
+      where: {
+        email: body.email
+      }
+    })
+    if (existeEmail) {
+      return res.status(400).json({
+        msg: 'Ya existe un usuario con el email'
+      })
+    }
+    const usuario = new Usuario(body)
+    await usuario.save();
+    return res.json(usuario)
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Hable con el administrador'
+    })
+  }
 
 }
 
-export const updatedUsuario = (req:Request, res:Response) => {
+export const updatedUsuario = async (req:Request, res:Response) => {
   const { id } = req.params;
   const { body } = req;
 
-  res.json({
-    msg: 'put Usuario',
-    body
-  })
+  try {
+    const idUserExist = await Usuario.findByPk(id);
+    if(!idUserExist){
+      return res.status(404).json({
+        msg:'No existe un usuario con el ID' + id
+      })
+    }
+    await idUserExist.update(body)
+    return res.json(idUserExist)
 
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: 'Hable con el administrador'
+    })
+  }
 }
 
 export const deleteUsuario = (req:Request, res:Response) => {
--- dist/controllers/usuarios.js
+++ dist/controllers/usuarios.js
@@ -1,37 +1,85 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUsuario = exports.updatedUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
-const getUsuarios = (req, res) => {
+const user_1 = __importDefault(require("../models/user"));
+const getUsuarios = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarios = yield user_1.default.findAll();
     res.json({
-        msg: 'Get Usuarios'
+        msg: 'Get Usuarios',
+        usuarios
     });
-};
+});
 exports.getUsuarios = getUsuarios;
-const getUsuario = (req, res) => {
+const getUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'Get Usuario',
-        id
-    });
-};
+    const usuario = yield user_1.default.findByPk(id);
+    if (usuario) {
+        return res.json(usuario);
+    }
+    else {
+        return res.status(404).json({
+            msg: `No existe un usuario con el ${id}`
+        });
+    }
+});
 exports.getUsuario = getUsuario;
-const postUsuario = (req, res) => {
+const postUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    console.log(req.body);
-    res.json({
-        msg: 'post Usuario',
-        body
-    });
-};
+    try {
+        const existeEmail = yield user_1.default.findOne({
+            where: {
+                email: body.email
+            }
+        });
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: 'Ya existe un usuario con el email'
+            });
+        }
+        const usuario = new user_1.default(body);
+        yield usuario.save();
+        return res.json(usuario);
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
+});
 exports.postUsuario = postUsuario;
-const updatedUsuario = (req, res) => {
+const updatedUsuario = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const { body } = req;
-    res.json({
-        msg: 'put Usuario',
-        body
-    });
-};
+    try {
+        const idUserExist = yield user_1.default.findByPk(id);
+        if (!idUserExist) {
+            return res.status(404).json({
+                msg: 'No existe un usuario con el ID' + id
+            });
+        }
+        yield idUserExist.update(body);
+        return res.json(idUserExist);
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
+});
 exports.updatedUsuario = updatedUsuario;
 const deleteUsuario = (req, res) => {
     const { id } = req.params;
