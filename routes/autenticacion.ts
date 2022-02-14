745f9e8b21a4a653566c9803c1381f5d4679d8f6
Luis Carrion <lcarrionalvarez@gmail.com>
2022-02-13 22:47:53 -0500

    Express validator added

--- routes/autenticacion.ts
+++ routes/autenticacion.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import {check} from 'express-validator';
 import { autenticacion } from "../controllers/autenticacion";
 
 const router = Router();
 
 router.post('/login',[
-  
+  check('nombre').notEmpty(),
+  check('password').notEmpty(),
 ],autenticacion);
 
 export default router;
